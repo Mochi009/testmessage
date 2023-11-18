861319aec28cb6318d1b1feb72a5e4ce9d183ad0
Mochi009 <s294949@studenti.polito.it>
2023-11-18 17:14:35 +0100

    Aggiunta variabiles ts e useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const srcIframe = 'http://localhost:3000/myd-solo/b11306be-09b9-456d-9407-de505283dc10/new-dashboard?orgId=1&from=1699535636799&to=1699557236799&panelId=1';
-  const sendMessageToGrafana = message => {
-    const grafanaIframe = document.getElementById('grafana-iframe');
-    grafanaIframe.contentWindow.postMessage(message, srcIframe);
-  };
+  const [ts, setTs] = useState('3h');
+  const srcIframe = 'http://localhost:3000/myd-solo/dfd49481-ca4a-45c6-a24c-726af667750c/new-dashboard?orgId=1&var-ts=3h&panelId=1';
+  useEffect(() => {
+    const sendMessageToGrafana = () => {
+      const message = {
+        "var-ts": ts,
+      };
+      const grafanaIframe = document.getElementById('grafana-iframe');
+      grafanaIframe.contentWindow.postMessage(message, srcIframe);
+    };
+    sendMessageToGrafana();
+  }, [ts]);
 
   return (
     <>
-      <DropdownMenu onChangeTime={sendMessageToGrafana} />
+      <DropdownMenu ts={ts} setTs={setTs} />
       <iframe title={'grafana'} id={'grafana-iframe'} src={srcIframe} width="100%" height={800}></iframe>
     </>
   );
 }
 
-const DropdownMenu = ({ onChangeTime }) => {
-  const [selectedOption, setSelectedOption] = useState('3h');
+const DropdownMenu = ({ ts, setTs }) => {
+  const [selectedOption, setSelectedOption] = useState(ts);
 
 
   const handleSelect = (eventKey) => {
     setSelectedOption(eventKey);
-    onChangeTime(eventKey);
+    setTs(eventKey);
   };
 
   return (
