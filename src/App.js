381c3f43bc74f5d5ec37b54113ddf54b9ed115f3
Mochi009 <s294949@studenti.polito.it>
2023-12-21 16:58:37 +0100

    Rimosso temp

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [ts, setTs] = useState('3h');
-  const [temp, setTemp] = useState('1');
   const [panelId, setPanelId] = useState('1');
-  const srcIframe = 'http://localhost:3000/myd-solo/f813b13e-a48f-4343-9f3e-c55906c5430b/new-dashboard?orgId=1&var-ts=3h&panelId=1';
-
+  const srcIframe = 'http://localhost:3000/myd-solo/c10d6588-f085-44d3-b9e6-9cf4043716ff/new-dashboard?orgId=1&var-ts=3h&panelId=1';
   useEffect(() => {
     const sendMessageToGrafana = () => {
       const message = {
         variables: [
-          { key: "ts", value: ts },
-          { key: "temp", value: ts },
-        ]
+          { key: "ts", value: ts },]
       };
       const grafanaIframe = document.getElementById('grafana-iframe');
       grafanaIframe.contentWindow.postMessage(message, srcIframe);
     };
     sendMessageToGrafana();
-  }, [ts, temp]);
+  }, [ts]);
 
   useEffect(() => {
     const sendMessageToGrafana = () => {
@@ -35,11 +31,13 @@ function App() {
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
+        Time Range:
         <DropdownMenuTs ts={ts} setTs={setTs} />
-        <DropdownMenuTemp temp={temp} setTemp={setTemp} />
+        Panel ID:
         <DropdownMenuPanelId panelId={panelId} setPanelId={setPanelId} />
       </div>
       <iframe title={'grafana'} id={'grafana-iframe'} src={srcIframe} width="100%" height={800}></iframe>
+
     </>
   );
 }
@@ -59,34 +57,13 @@ const DropdownMenuTs = ({ ts, setTs }) => {
         {selectedOption}
       </Dropdown.Toggle>
       <Dropdown.Menu>
+        <Dropdown.Item eventKey="1h">1h</Dropdown.Item>
+        <Dropdown.Item eventKey="2h">2h</Dropdown.Item>
         <Dropdown.Item eventKey="3h">3h</Dropdown.Item>
+        <Dropdown.Item eventKey="4h">4h</Dropdown.Item>
         <Dropdown.Item eventKey="6h">6h</Dropdown.Item>
         <Dropdown.Item eventKey="12h">12h</Dropdown.Item>
         <Dropdown.Item eventKey="1d">1d</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
-};
-
-const DropdownMenuTemp = ({ temp, setTemp }) => {
-  const [selectedOption, setSelectedOption] = useState(temp);
-
-
-  const handleSelect = (eventKey) => {
-    setSelectedOption(eventKey);
-    setTemp(eventKey);
-  };
-
-  return (
-    <Dropdown onSelect={handleSelect}>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        {selectedOption}
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item eventKey='1'>1</Dropdown.Item>
-        <Dropdown.Item eventKey="2">2</Dropdown.Item>
-        <Dropdown.Item eventKey="3">3</Dropdown.Item>
-        <Dropdown.Item eventKey="4">4</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
